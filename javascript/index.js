27143b65d90cc38e7df90f9fa9c53319512445f5
hollsmith2000 <hollsmith2002@hotmail.co.uk>
2023-02-12 21:50:09 +0000

    Project

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,20 @@
 function updateTime() {
-  let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
+  let positanoElement = document.querySelector("#positano");
+  let positanoDateElement = positanoElement.querySelector(".date");
+  let positanoTimeElement = positanoElement.querySelector(".time");
+  let positanoTime = moment().tz("CET");
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("Do MMMM YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+  positanoDateElement.innerHTML = positanoTime.format("Do MMMM YYYY");
+  positanoTimeElement.innerHTML = positanoTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
+  let newOrleansElement = document.querySelector("#new-orleans");
+  let newOrleansDateElement = newOrleansElement.querySelector(".date");
+  let newOrleansTimeElement = newOrleansElement.querySelector(".time");
+  let newOrleansTime = moment().tz("US/Central");
+
+  newOrleansDateElement.innerHTML = newOrleansTime.format("Do MMMM YYYY");
+  newOrleansTimeElement.innerHTML = newOrleansTime.format(
     "hh:mm:ss [<small>]A[</small>]"
   );
   let sydneyElement = document.querySelector("#sydney");
@@ -36,7 +45,8 @@ function updateCity(event) {
           <div class="time">${cityTime.format(
             "h:mm:ss"
           )} <small>${cityTime.format("A")} </small> </div>
-        </div>`;
+        </div>
+        <a href="/">All cities</a>`;
 }
 
 updateTime();
